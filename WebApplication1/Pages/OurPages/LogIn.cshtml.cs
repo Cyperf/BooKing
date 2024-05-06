36d10ede4cb1d55dc9fbfab280e99cb945866802
Roman Ghazi <rog001@edu.zealand.dk>
2024-05-06 13:47:09 +0200

    Log in (Login.cshtml.cs)

--- WebApplication1/Pages/OurPages/LogIn.cshtml.cs
+++ WebApplication1/Pages/OurPages/LogIn.cshtml.cs
@@ -1,12 +1,70 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
+using WebApplication1.Models;
 
-namespace WebApplication1.Pages.OurPages
+namespace WebApplication1.Pages.OurPages //* Lavet af Roman
 {
     public class LogInModel : PageModel
     {
-        public void OnGet()
+        // private IUserDataService _userDataService;
+        public static Bruger LoggedInBruger { get; set; }
+
+        [BindProperty]
+
+        public string Email { get; set; }
+
+        [BindProperty]
+        public string Kodeord { get; set; }
+
+        [BindProperty]
+        public string Message { get; set; }
+
+       // [BindProperty]
+       // public BrugerRolle Rolle { get; set; }
+
+        public async Task<IActionResult> OnPost()
         {
+
+
+            if (!WebApplication1.Services.LoginManager.Login(Email, Kodeord))
+            {
+                Message = "Fejl: Invalid login";
+                return Page();
+            }
+            LoggedInBruger = WebApplication1.Services.LoginManager.LoggedInUser;
+
+
+
+            //if (LoggedInBruger == null)
+            //{
+            //    Message = "Fejl: Invalid login";
+            //    return Page();
+            //}
+
+            //Login med identity - note til mig selv
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                  BuildClaimsPrincipalFromUser(LoggedInBruger));
+
+            return RedirectToPage("BrugerSide");
+
+        }
+        private ClaimsPrincipal BuildClaimsPrincipalFromUser(Bruger bruger)
+        {
+            List<Claim> Claims = new List<Claim>
+            {
+            new Claim(ClaimTypes.Email, bruger.Email),
+            new Claim(ClaimTypes.Role, bruger.Rolle.RolleNavn),
+            };
+
+            ClaimsIdentity ClaimsIdentity = new ClaimsIdentity(Claims,
+            CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return new ClaimsPrincipal(ClaimsIdentity);
         }
     }
 }
+
