5512791601203bc7a40258b1b60babb8b32db970
Jeppe August Hirsch Jeppsson <jeppe.jeppsson@live.dk>
2024-05-07 12:28:16 +0200

    Logging in works

--- WebApplication1/Services/LoginManager.cs
+++ WebApplication1/Services/LoginManager.cs
@@ -1,22 +1,30 @@
 ﻿namespace WebApplication1.Services
 {
-	using WebApplication1.Models;
-	using WebApplication1.SQL;
+    using WebApplication1.Models;
+    using WebApplication1.SQL;
 	public class LoginManager
 	{
 		public static WebApplication1.Models.Bruger? LoggedInUser { get; private set; } = null;
 		public static bool Login(string email, string password)
 		{
-			string hashedPassword = HashPassword(email, password);
-			bool b = false;
-			AdoNet.ExecuteQuery($"SELECT Name, Email, Kode, Rolle, SkoleId, SletningsDato FROM bruger WHERE Email={email} AND Kode={hashedPassword}",
+			var hasher = new Microsoft.AspNetCore.Identity.PasswordHasher<string>();
+            //string hashedPassword = hasher.HashPassword(email, password);
+
+            bool b = false;
+
+			AdoNet.ExecuteQuery($"SELECT Name, Email, Kode, Rolle, SkoleId, SletningsDato FROM bruger WHERE Email='{email}'",
 				reader =>
 				{
-					if (!reader.Read())
+                    System.Diagnostics.Debug.WriteLine($"Attempted to login with {email}, {password}");
+                    if (!reader.Read())
+						return;
+					var result = hasher.VerifyHashedPassword(email, reader.GetString(2), password);
+                    if (result != Microsoft.AspNetCore.Identity.PasswordVerificationResult.Success)
 						return;
 					b = true;
 					LoggedInUser = new Bruger(reader.GetString(0), reader.GetString(1), reader.GetString(2), GetUserRoleFromId(reader.GetInt32(3)), reader.GetInt32(4), DateOnly.FromDateTime(reader.GetDateTime(5)));
-					});
+					System.Diagnostics.Debug.WriteLine("Logged in as:\n" + LoggedInUser);	
+				});
 			return b;
 		}
 
@@ -31,8 +39,12 @@
 				});
 			return rolle;
 		}
+        //public static string HashPassword(string password)
+        //{
+        //    return new Microsoft.AspNetCore.Identity.PasswordHasher<string>().HashPassword(password);
+        //}
 
-		public static string HashPassword(string email, string password)
+        public static string HashPassword(string email, string password)
 		{
 			return new Microsoft.AspNetCore.Identity.PasswordHasher<string>().HashPassword(email, password);
         }
