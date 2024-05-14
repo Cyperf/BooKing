874d545a29ce9ab766efceb1bb4b135dac1f614e
Cyperf <drageon@live.dk>
2024-05-14 11:34:17 +0200

    asd

--- WebApplication1/Pages/OurPages/AdminRedigeringAfBruger.cshtml.cs
+++ WebApplication1/Pages/OurPages/AdminRedigeringAfBruger.cshtml.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication1.Models;
+using WebApplication1.Services;
 
 namespace WebApplication1.Pages.OurPages
 {
     public class AdminRedigeringAfBrugerModel : PageModel
     {
+        public Bruger bruger {  get; set; } = new Bruger();
+        public BrugerService brugerService { get; set; } = new BrugerService();
+
         public void OnGet()
         {
+
+            brugerService = new BrugerService();
+        }
+
+        public IActionResult OnPostSletBruger(Bruger bruger)
+        {
+            brugerService.DeleteByEmail(bruger.Email);
+            return Page();
         }
     }
 }
